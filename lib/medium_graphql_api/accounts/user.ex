b10a727ee509dc11bfec44045b05f8a97541c7ff
Christopher Rijos <rijos.christopher@gmail.com>
2019-03-01 18:35:26 -0600

    wip

--- lib/medium_graphql_api/accounts/user.ex
+++ lib/medium_graphql_api/accounts/user.ex
@@ -4,11 +4,13 @@ defmodule MediumGraphqlApi.Accounts.User do
 
 
   schema "users" do
-    field :email, :string
-    field :first_name, :string
-    field :last_name, :string
-    field :password_hash, :string
-    field :role, :string
+    field(:email, :string, unique: true)
+    field(:first_name, :string)
+    field(:last_name, :string)
+    field(:password_hash, :string)
+    field(:password, :string, virtual: true)
+    field(:password_confirmation, :string, virtual: true)
+    field(:role, :string, default: "user")
 
     timestamps()
   end
@@ -17,6 +19,25 @@ defmodule MediumGraphqlApi.Accounts.User do
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:first_name, :last_name, :email, :password_hash, :role])
-    |> validate_required([:first_name, :last_name, :email, :password_hash, :role])
+    |> validate_required([
+      :first_name, 
+      :last_name, 
+      :email, 
+      :password_hash, 
+      :role
+    ])
+    |> validate_format(:email, ~r/@/) 
+    |> update_change(:email, &String.downcase(&1))
+    |> validate_length(:password, min: 6, max: 100)
+    |> validate_confirmation(:password)
+    |> hash_password
+  end
+
+  defp hash_password(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do 
+    change(changeset, Comeonin.Argon2.add_hash(password))
+  end
+
+  defp hash_password(changeset) do 
+    changeset
   end
 end
--- mix.exs
+++ mix.exs
@@ -40,7 +40,9 @@ defmodule MediumGraphqlApi.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:plug_cowboy, "~> 1.0"}
+      {:plug_cowboy, "~> 1.0"},
+      {:comeonin, "~> 4.0"}, 
+      {:argon2_elixir, "~> 1.2"},
     ]
   end
 
